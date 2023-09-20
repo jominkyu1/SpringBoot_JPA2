eb6be696dfb39c5e7631c4aa0d5febd2fa49dfb6
jominkyu1 <jominkyu@gmail.com>
2023-09-20 21:40:55 +0900

    0920 Spring Boot JPA 프로젝트 생성

---
+++ src/main/java/net/daum/vo/MemberVO.java
@@ -0,0 +1,27 @@
+package net.daum.vo;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Setter
+@Getter
+@ToString
+@Entity
+@Table(name="tbl_members")
+@EqualsAndHashCode(of="uid2")
+// equals(), hashCode(), canEqual() 메서드 자동 생성
+public class MemberVO { //회원 Entity빈 클래스
+
+    @Id //엔티티 빈을 식별할 수 있도록 해주는 식별키(구분키) : PK
+    private String uid2; //회원아이디
+
+    private String upw; //비밀번호
+    
+    private String uname; //회원이름
+}
---
+++ src/main/java/net/daum/vo/Profile.java
@@ -0,0 +1,9 @@
+package net.daum.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Profile {
+}
